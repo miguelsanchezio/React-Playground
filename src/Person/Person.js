5182e0b953dbdb4de88f1643cc4ddf2bb98a03d0
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-03-12 21:23:53 -0700

    Started outputting dynamic content.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Person = () => {
-  return <p>I'm a Person component.</p>;
+  return <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old.</p>;
 }
 
 export default Person;
