164ab1b1179c627b0194b67745bed23520d9a9a8
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-03-13 21:08:18 -0700

    Added state to App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Miguel', age: 24 },
+      { name: 'Ana', age: 23 },
+      { name: 'Yesi', age: 28 },      
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi.</h1>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-        <Person name="Miguel" age="24"/>
-        <Person name="Ana" age="23">My Hobbies: Racing</Person>
-        <Person name="Steph" age="25"/>        
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>   
       </div>
     );
   }
