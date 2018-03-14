c12ab0ec02e290ac3c3904c0887914b85d8380a1
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-03-14 15:03:46 -0700

    Added styles with stylesheets and inline styles.

--- src/App.js
+++ src/App.js
@@ -35,11 +35,19 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid orange',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>Hi.</h1>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-        <button onClick={() => this.switchNameHandler('Alejandro')}>Switch Name</button>
+        <button style={style} onClick={() => this.switchNameHandler('Alejandro')}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}/>
